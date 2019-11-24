3e34cf33de93cbdd19a22b622618a80fb89ee901
iron2014hieu <55474568+iron2014hieu@users.noreply.github.com>
2019-11-24 10:50:59 +0700

    upp đét

--- app/src/main/java/iron2014/bansachonline/LoginRegister/LoginActivity.java
+++ app/src/main/java/iron2014/bansachonline/LoginRegister/LoginActivity.java
@@ -210,23 +210,23 @@ public class LoginActivity extends AppCompatActivity {
 
         final String email = edtEmail.getText().toString();
 
-//        if (token == null) {
-//            progressDialog.dismiss();
-//            Toast.makeText(this, "Token not generated", Toast.LENGTH_LONG).show();
-//            return;
-//        }
+        if (token == null) {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Token not generated", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, EndPoints.URL_REGISTER_DEVICE,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         progressDialog.dismiss();
-//                        try {
-//                            JSONObject obj = new JSONObject(response);
-//                            Toast.makeText(LoginActivity.this, obj.getString("message"), Toast.LENGTH_LONG).show();
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            Toast.makeText(LoginActivity.this, obj.getString("message"), Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
